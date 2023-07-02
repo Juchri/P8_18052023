b62c238ec34c5b92b3effbf21f40d4f31dfc8e40
Juchri <juliette.chri@gmail.com>
2023-07-02 22:27:17 +0200

    ⚡️test listAction without import login

--- tests/Controller/TaskControllerTest.php
+++ tests/Controller/TaskControllerTest.php
@@ -16,17 +16,35 @@ class TaskControllerTest extends WebTestCase
         $this->router = static::$kernel->getContainer()->get('router');
     }
 
-    public function testHomepageIsUp()
+    public function testListAction()
     {
-        $crawler = $this->client->request(Request::METHOD_GET, $this->router->generate('homepage'));
-        $this->assertTrue($this->client->getResponse()->isRedirect());
-        
-        $crawler = $this->client->followRedirect();
 
-        // Vérifier si le crawler a trouvé des nœuds
-        $this->assertNotEmpty($crawler->filter('h1'));
+        $client = static::createClient();
 
-        // Vérifier si le texte "Login" est présent dans la balise h1
-        $this->assertStringContainsStringIgnoringCase('Login', $crawler->filter('h1')->text());
+        // Accède à la page de connexion
+        $crawler = $client->request('GET', '/login');
+
+        $usernametotest =  'admin';
+
+        // Remplis le formulaire avec des identifiants valides
+        $form = $crawler->selectButton('Se connecter')->form();
+        $form['_username'] = $usernametotest;
+        $form['_password'] = 'admin';
+
+        // Soumets le formulaire
+        $crawler = $client->submit($form);
+
+        // Vérifie la redirection après connexion réussie
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        $this->assertEquals('http://localhost/', $client->getResponse()->headers->get('Location'));
+
+        // Vérifie la présence d'un message de bienvenue dans la page d'accueil
+        $crawler = $client->followRedirect();
+
+        $this->assertStringContainsStringIgnoringCase( $usernametotest, $crawler->filter('.welcome-message')->text());
+
+        $crawler = $client->request('GET', '/tasks');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
+
 }
