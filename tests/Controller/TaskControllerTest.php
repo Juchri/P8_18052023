c1b8fd8f50af3e5409ff1675ab40a247451c54ac
Juchri <juliette.chri@gmail.com>
2023-07-04 20:27:46 +0200

    ⚡️ taskController tests ok (but with id put manually for deleting)

--- tests/Controller/TaskControllerTest.php
+++ tests/Controller/TaskControllerTest.php
@@ -83,7 +83,6 @@ class TaskControllerTest extends WebTestCase
         $currentUser = $client->getContainer()->get('security.token_storage')->getToken()->getUser();
         // Utilisez $currentUser comme utilisateur connecté dans vos assertions et vos tests
 
-
         // Accéder à la page de création de tâche
         $crawler = $client->request('GET', '/tasks/create');
 
@@ -159,4 +158,141 @@ class TaskControllerTest extends WebTestCase
         $this->assertEquals('Nouvelle description', $updatedTask->getContent());
     }
 
+    public function testEditActionUnauthorized()
+    {
+        // Créer un client de test
+        $client = static::createClient();
+
+         // Se connecter avec un utilisateur fictif
+         new User; // Créez un utilisateur ou utilisez un utilisateur existant
+         $crawler = $client->request('GET', '/login');
+         // Soumettez le formulaire de connexion avec les informations d'identification valides
+         $form = $crawler->selectButton('Se connecter')->form([
+             '_username' => 'admin',
+             '_password' => 'admin',
+         ]);
+         $client->submit($form);
+
+        // Récupérer une tâche existante depuis la base de données mais qui n'est pas faite par le même user
+        $em = $client->getContainer()->get('doctrine')->getManager();
+        $task = $em->getRepository(Task::class)->findOneBy(['id' => 8]);
+
+        // Accéder à la page d'édition de la tâche
+        $crawler = $client->request('GET', '/tasks/'.$task->getId().'/edit');
+
+        // Vérifier que le flash message est affiché
+        $crawler = $client->followRedirect();
+        $flashMessage = $crawler->filter('.alert-danger')->text();
+        $this->assertStringContainsString('Oops', $flashMessage);
+    }
+
+    public function testToggleTaskAction()
+    {
+        $client = static::createClient();
+
+        // Se connecter avec un utilisateur fictif
+        new User; // Créez un utilisateur ou utilisez un utilisateur existant
+        $crawler = $client->request('GET', '/login');
+        // Soumettez le formulaire de connexion avec les informations d'identification valides
+        $form = $crawler->selectButton('Se connecter')->form([
+            '_username' => 'admin',
+            '_password' => 'admin',
+        ]);
+        $client->submit($form);
+
+        // Récupérer une tâche existante depuis la base de données
+        $task = $client->getContainer()->get('doctrine')->getRepository(Task::class)->find(1);
+
+        // Effectuer une requête POST vers l'URL de la fonction toggleTaskAction avec l'ID de la tâche
+        $client->request('POST', '/tasks/'.$task->getId().'/toggle');
+
+        // Vérifier que la réponse est une redirection vers la liste des tâches
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        $this->assertEquals('/tasks', $client->getResponse()->headers->get('location'));
+
+        // Récupérer la tâche mise à jour depuis la base de données
+        $updatedTask = $client->getContainer()->get('doctrine')->getRepository(Task::class)->find(1);
+
+        // Vérifier que l'état de la tâche a été inversé
+        $this->assertEquals(!$task->isDone(), $updatedTask->isDone());
+
+        // Vérifier le message flash affiché
+        $crawler = $client->followRedirect();
+        $flashMessage = $crawler->filter('.alert-success')->text();
+        $expectedFlashMessage = sprintf('Superbe ! La tâche %s a bien été marquée comme faite.', $updatedTask->getTitle());
+        $this->assertEquals($expectedFlashMessage, trim($flashMessage));
+    }
+
+    public function testDeleteTaskAction()
+    {
+    // Créer un client de test
+    $client = static::createClient();
+
+    // Se connecter avec un utilisateur fictif
+    new User; // Créez un utilisateur ou utilisez un utilisateur existant
+    $crawler = $client->request('GET', '/login');
+    // Soumettez le formulaire de connexion avec les informations d'identification valides
+    $form = $crawler->selectButton('Se connecter')->form([
+        '_username' => 'admin',
+        '_password' => 'admin',
+    ]);
+    $client->submit($form);
+
+    // Créer une nouvelle tâche
+    $task = new Task();
+    $currentUser = $client->getContainer()->get('security.token_storage')->getToken()->getUser();
+    $idToDelete = 71;
+    // Définir les propriétés de la tâche selon vos besoins
+    $task->setTestId($idToDelete);
+    $task->setTitle('Tâche à supprimer');
+    $task->setContent('Tâche à supprimer');
+    $task->setUser($currentUser);
+
+    $em = $client->getContainer()->get('doctrine')->getManager();
+
+    // Persistez et flush la tâche
+    $em->persist($task);
+    $em->flush();
+
+    // Effectuer une requête POST vers l'URL de la fonction deleteTaskAction avec l'ID de la tâche
+    $client->request('POST', '/tasks/'.$idToDelete.'/delete');
+
+     // Vérifier que la réponse est une redirection vers la liste des tâches
+     $this->assertEquals(302, $client->getResponse()->getStatusCode());
+     $this->assertEquals('/tasks', $client->getResponse()->headers->get('location'));
+
+     // Vérifier que la tâche a été supprimée de la base de données
+     $deletedTask = $client->getContainer()->get('doctrine')->getRepository(Task::class)->find($idToDelete);
+     $this->assertNull($deletedTask);
+
+     // Vérifier le message flash affiché
+     $crawler = $client->followRedirect();
+     $flashMessage = $crawler->filter('.alert-success')->text();
+     $this->assertEquals('Superbe ! La tâche a bien été supprimée.', trim($flashMessage));
+    }
+
+    public function testDeleteTaskActionUnauthorized()
+    {
+    // Créer un client de test
+    $client = static::createClient();
+
+    // Se connecter avec un utilisateur fictif
+    new User; // Créez un utilisateur ou utilisez un utilisateur existant
+    $crawler = $client->request('GET', '/login');
+    // Soumettez le formulaire de connexion avec les informations d'identification valides
+    $form = $crawler->selectButton('Se connecter')->form([
+        '_username' => 'admin',
+        '_password' => 'admin',
+    ]);
+    $client->submit($form);
+
+    $idToDelete = 8;
+    $client->request('POST', '/tasks/'.$idToDelete.'/delete');
+
+    // Vérifier le message flash affiché
+    $crawler = $client->followRedirect();
+    $flashMessage = $crawler->filter('.alert-danger')->text();
+    $this->assertEquals('Oops ! Cette tâche ne peut être suprimée que par son auteur ou un admin.', trim($flashMessage));
+
+    }
 }
